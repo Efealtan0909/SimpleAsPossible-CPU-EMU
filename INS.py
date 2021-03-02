b0e21a658dc4c226472720704cae8e4dab60938f
EfeAltan0909 <60935052+Efealtan0909@users.noreply.github.com>
2021-03-02 20:32:50 +0300

    Added subroutines Fixed MOV not moving a value to a RAM adress directly

--- INS.py
+++ INS.py
@@ -3,7 +3,11 @@ import ROM
 import time
 import threading
 
-# INSTABLE
+## RAM ALLOCATION
+# 0-16   GENERAL USAGE
+# 17-24  SUBROUTINE LOCATIONS
+
+## INSTABLE
 # NOP
 # MOV 
 # ADD 
@@ -15,6 +19,8 @@ import threading
 # JNE
 # JGT
 # JLT
+# JSR
+# RSR
 # BOP
 # END / HALT
 
@@ -24,6 +30,7 @@ RBD  = "0"      # B           Register (GPR)
 ACC  = "0"      # Accumulator Register (ALU)
 BOR  = "0"      # Binary OP   Register (BOP)
 LRD  = "0"      # Line        Register (INS) 
+CSR  = "0"
 INSR = ""       # Instruction Register (INS)  
 
 # VARS
@@ -49,6 +56,7 @@ def INFLOOP():
     global ACC
     global BOR
     global LRD
+    global CSR
     global INSR
     INSR = ROM.Read(LRD)
     INSS = INSR.split(' ')
@@ -65,37 +73,40 @@ def INFLOOP():
             elif INSS[2].find('ACC') == 0:
                 ACC = RAM.Read(INSS[1].replace('!', ''))
         else:
-            if INSS[1].find('RA') == 0:
-                if INSS[2][0] == '!':
-                    RAM.Write(INSS[2].replace('!', ''), RAD)
-                else:
-                    if INSS[2].find('RB') == 0:
-                        RBD = RAD
-                    elif INSS[2].find('ACC') == 0:
-                        ACC = RAD
-            elif INSS[1].find('RB') == 0:
-                if INSS[2][0] == '!':
-                    RAM.Write(INSS[2].replace('!', ''), RBD)
-                else:
-                    if INSS[2].find('RA') == 0:
-                        RAD = RBD
-                    elif INSS[2].find('ACC') == 0:
-                        ACC = RBD
-            elif INSS[1].find('ACC') == 0:
-                if INSS[2][0] == '!':
-                    RAM.Write(INSS[2].replace('!', ''), ACC)
-                else:
-                    if INSS[2].find('RA') == 0:
-                        RAD = ACC
-                    elif INSS[2].find('RB') == 0:
-                        RBD = ACC
+            if INSS[2][0] == '!':
+                RAM.Write(INSS[2].replace('!', ''), INSS[1])
             else:
-                if INSS[2].find('RA') == 0:
-                    RAD = INSS[1]
-                elif INSS[2].find('RB') == 0:
-                    RBD = INSS[1]
-                elif INSS[2].find('ACC') == 0:
-                    ACC = INSS[1]
+                if INSS[1].find('RA') == 0:
+                    if INSS[2][0] == '!':
+                        RAM.Write(INSS[2].replace('!', ''), RAD)
+                    else:
+                        if INSS[2].find('RB') == 0:
+                            RBD = RAD
+                        elif INSS[2].find('ACC') == 0:
+                            ACC = RAD
+                elif INSS[1].find('RB') == 0:
+                    if INSS[2][0] == '!':
+                        RAM.Write(INSS[2].replace('!', ''), RBD)
+                    else:
+                        if INSS[2].find('RA') == 0:
+                            RAD = RBD
+                        elif INSS[2].find('ACC') == 0:
+                            ACC = RBD
+                elif INSS[1].find('ACC') == 0:
+                    if INSS[2][0] == '!':
+                        RAM.Write(INSS[2].replace('!', ''), ACC)
+                    else:
+                        if INSS[2].find('RA') == 0:
+                            RAD = ACC
+                        elif INSS[2].find('RB') == 0:
+                            RBD = ACC
+                else:
+                    if INSS[2].find('RA') == 0:
+                        RAD = INSS[1]
+                    elif INSS[2].find('RB') == 0:
+                        RBD = INSS[1]
+                    elif INSS[2].find('ACC') == 0:
+                        ACC = INSS[1]
     
     elif OPCODE.find('ADD') == 0:
         OPERATION = "ADD"
@@ -217,6 +228,18 @@ def INFLOOP():
                 else:
                     LRD = str(int(LRD) + 1)
     
+    elif OPCODE.find('JSR') == 0:
+        OPERATION = "JSR"
+        if int(CSR) < 9:
+            RAM.Write(str(17+int(CSR)), str(int(LRD) + 1))
+            LRD = INSS[1]
+            CSR = str(int(CSR) + 1)
+    elif OPCODE.find('RSR') == 0:
+        OPERATION = "RSR"
+        if int(CSR) < 9 and int(CSR) > 0:
+            print(CSR)
+            LRD = RAM.Read(str(17+(int(CSR)-1)))
+            CSR = str(int(CSR) - 1)
     elif OPCODE.find('BOP') == 0:
         OPERATION = "BOP"
         if INSS[1] == 'AND':
@@ -250,6 +273,7 @@ def INFLOOP():
     print('ACC      | '+ACC)
     print('BOR      | '+BOR)
     print('LRD      | '+LRD)
+    print('CSR      | '+CSR)
     print('INSR     | '+INSR)
     print('\nOPERATION INFO\n')
     print('OP       | '+INSR)
@@ -257,7 +281,7 @@ def INFLOOP():
     print('INS      | '+OPERATION)
     print('\nRAM INFO\n')
     COUNTERD = "0"
-    while int(COUNTERD) <= 15:
+    while int(COUNTERD) <= 24:
         if int(COUNTERD) > 9:
             print(COUNTERD+' : '+RAM.Read(COUNTERD))
         else:
@@ -267,7 +291,7 @@ def INFLOOP():
     if not LOOP:
         print('\n')
     else:
-        if OPERATION != "JMP" and OPERATION != "JE" and OPERATION != "JNE" and OPERATION != "JGT" and OPERATION != "JLT":
+        if OPERATION != "JMP" and OPERATION != "JE" and OPERATION != "JNE" and OPERATION != "JGT" and OPERATION != "JLT" and OPERATION != "JSR" and OPERATION != "RSR":
             LRD = str(int(LRD) + 1)
-
     return LOOP
+    
